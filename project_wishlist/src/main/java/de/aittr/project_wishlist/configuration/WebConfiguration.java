9b88a2de9f523a2ff77c7b912e43795785a38e61
Таня Самойленко <ania000535@gmail.com>
2025-10-31 11:55:41 +0100

    back

--- project_wishlist/src/main/java/de/aittr/project_wishlist/configuration/WebConfiguration.java
+++ project_wishlist/src/main/java/de/aittr/project_wishlist/configuration/WebConfiguration.java
@@ -2,24 +2,13 @@ package de.aittr.project_wishlist.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")  // можно указать адрес React: http://localhost:3000
-                .allowedMethods("*")
-                .allowedHeaders("*");
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        // 👇 Исправленный вариант без регулярных выражений
-        registry.addViewController("/{path:[^\\.]*}")
-                .setViewName("forward:/index.html");
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
